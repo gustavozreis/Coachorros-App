c3410e038997770aac69efefb9c6fb42eb78dc24
gustavozreis <gzreis@gmail.com>
2022-03-14 13:59:20 -0300

    inicio implementação retrofit

---
+++ app/src/main/java/com/gustavozreis/dogvacionalapp/network/DogApiService.kt
@@ -0,0 +1,6 @@
+package com.gustavozreis.dogvacionalapp.network
+
+private const val BASE_URL = "https://dog.ceo/api/breeds/image/random"
+
+interface DogApiService {
+}
---
+++ app/src/main/java/com/gustavozreis/dogvacionalapp/network/DogPhotoModel.kt
@@ -0,0 +1,6 @@
+package com.gustavozreis.dogvacionalapp.network
+
+data class DogPhotoModel (
+    val message: String,
+    val status: String
+        )
